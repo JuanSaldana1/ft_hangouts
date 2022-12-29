38b7ba610cf01f0404729c1f6a43b1635ec152ad
jsaldana <saldana.juan.2002@gmail.com>
2022-12-29 15:01:09 +0100

    More modification of names. Creating item contact view

--- app/src/main/java/com/jsaldana/ft_hangouts/ListContactsFragment.kt
+++ app/src/main/java/com/jsaldana/ft_hangouts/ListContactsFragment.kt
@@ -1,19 +1,18 @@
 package com.jsaldana.ft_hangouts
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.jsaldana.ft_hangouts.databinding.FragmentFirstBinding
+import androidx.fragment.app.Fragment
+import com.jsaldana.ft_hangouts.databinding.FragmentListContactsBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class ListContactsFragment : Fragment() {
 
-	private var _binding: FragmentFirstBinding? = null
+	private var _binding: FragmentListContactsBinding? = null
 
 	// This property is only valid between onCreateView and
 	// onDestroyView.
@@ -24,7 +23,7 @@ class FirstFragment : Fragment() {
 		savedInstanceState: Bundle?
 	): View? {
 
-		_binding = FragmentFirstBinding.inflate(inflater, container, false)
+		_binding = FragmentListContactsBinding.inflate(inflater, container, false)
 		return binding.root
 
 	}
@@ -32,9 +31,6 @@ class FirstFragment : Fragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
-		binding.buttonFirst.setOnClickListener {
-			findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-		}
 	}
 
 	override fun onDestroyView() {
