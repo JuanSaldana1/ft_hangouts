ef13898148d34fb4f310f4afae8d01bffa6d88a0
jsaldana <saldana.juan.2002@gmail.com>
2022-12-29 19:29:53 +0100

    Select contact profile image works

--- app/src/main/java/com/jsaldana/ft_hangouts/CreateContactFragment.kt
+++ app/src/main/java/com/jsaldana/ft_hangouts/CreateContactFragment.kt
@@ -1,5 +1,8 @@
 package com.jsaldana.ft_hangouts
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,6 +14,8 @@ import com.jsaldana.ft_hangouts.databinding.FragmentCreateContactBinding
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class CreateContactFragment : Fragment() {
+	private val SELECTCODE = 1
+	private var pictureUri: Uri? = null
 
 	private var _binding: FragmentCreateContactBinding? = null
 
@@ -24,12 +29,29 @@ class CreateContactFragment : Fragment() {
 	): View {
 
 		_binding = FragmentCreateContactBinding.inflate(inflater, container, false)
+		selectProfileImage()
 		return binding.root
 
 	}
 
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+		when {
+			requestCode == SELECTCODE && resultCode == Activity.RESULT_OK -> {
+				pictureUri = data!!.data
+				binding.contactPictureImageView.setImageURI(pictureUri)
+			}
+		}
+	}
+
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
+	}
+
+	private fun selectProfileImage() {
+		binding.contactPictureImageView.setOnClickListener {
+			ImageController.selectPicture(this, SELECTCODE)
+		}
 	}
 
 	override fun onDestroyView() {
